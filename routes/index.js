08c72a72293d825907f2bd73c64b11d33d5842b4
reasonablep <140934359+reasonablep@users.noreply.github.com>
2023-11-09 18:05:21 -0800

    Fixed delete route

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 // Require express, define router
-// const express = require('express');
 const router = require('express').Router();
 const notesRouter = require('./notes.js');
 
-// Define app for export
-// const app = express();
-
-// Apply router path
-// app.use('/notes', notesRouter);
+// Define router file path
 router.use('/notes', notesRouter);
 
 // Export app
--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,7 @@
 const notesRoute = require('express').Router();
 const {readFromFile, readAndAppend} = require ('../helpers/fsutils');
 const {v4: uuidv4} = require('uuid');
-// const express = require('express');
-
-// Body parser provides req.body
-
-// notesRoute.use(express.json());
-// notesRoute.use(express.urlencoded({extended: true}));
+const fs = require ('fs');
 
 // Get notes from db.json when the root directory is requested
 
@@ -40,17 +35,39 @@ if (title && text) {
 
 // Delete note when ID matches the ID clicked in the event handler in /public/assets/js/index.js
 
-//splice or filter for delete route w/ for loop
+
 
 notesRoute.delete(`/:id`, (req, res) => {
-console.log('hello')
+const noteId = req.params.id;
 console.log(req.params.id)
-if (req.method === 'delete') {
-    res.json(`Note deleted`)
-}
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Error reading the database' })
+        }
+    
+        let notes = JSON.parse(data);
+        const index = notes.findIndex(note => note.id === noteId);
 
-})
+        if (index === -1) {
+            return res.status(404).json({ error: 'Note not found' })
+        }
 
+        notes.splice(index, 1);
+
+        fs.writeFile('./db/db.json', JSON.stringify(notes, null, 5), err => {
+
+            if (err) {
+                return res.status(500).json({ error: 'Error updating the database' });
+
+            }
+
+            res.json({message: 'Note deleted'})
+            
+        });
+
+    });
+
+});
 
 
 module.exports = notesRoute;
